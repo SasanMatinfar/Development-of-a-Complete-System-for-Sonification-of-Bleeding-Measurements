11c1983e2746ffbe95feffc7fc366aca2c3d3fab
Matthias Seibold <matthias.seibold@tum.de>
2019-04-09 11:51:26 +0200

    volume calculation

--- read_arduino.py
+++ read_arduino.py
@@ -1,36 +1,55 @@
 # import packages
 import serial
-import time
+import math
 
 # initialization
-timestr = time.strftime("%Y%m%d-%H%M%S")
-output_file = open("C:/Users/MITI-Sono/Google Drive/Master Maschinenwesen/Masterarbeit/Python/" + timestr + "_data.csv", "w+");
-sobj = serial.Serial('COM3', 9600)
-running_pre = 0;
+sobj = serial.Serial('COM5', 115200)
+x_sensor = 333.16
+max_volume = 0
+output_volume = 0
+d_volume_old = 0
+dd_volume = 0
+d_volume = 0
+
+
+def diameter_x(x):
+    output = (x_sensor-x)/335*(122.5-109.1)+109.1
+    return output
+
+
+def get_correction(d_volume, correction_factor=1):
+    return d_volume * correction_factor
+
 
 # run data acquisition loop
 while True:
- 
-    # read serial message until \n 
-    line = sobj.readline();
-    # convert to string
-    line = line.decode("utf-8")
 
-    # read identifier
-    running = float(line[0]);
-    print(line);
+    # read the distance from HC-SR04
+    distance = sobj.readline()
+    distance = float(distance.decode("utf-8"))
+    print(distance)
 
-    # start writing data to file when switch is activated
-    if running == 1:
-        output_file.write(line);
+    # fluid volume
+    if distance < x_sensor:
 
-    # stop writing data when switch is deactivated 
-    elif (running_pre - running) == 1:
-        output_file.close();
-        break
+        # for medela PSU 3L
+        volume = 1/3 * math.pi * (x_sensor - distance) * ((109.1/2) ** 2 + 109.1/2 * diameter_x(distance)/2 + (diameter_x(distance)/2) ** 2)
+        volume_ml = 1e-3 * volume
 
-    # write variable for next iteration
-    running_pre = running;
-	
-	
-	
+        if volume_ml > max_volume:
+            d_volume = volume_ml - max_volume
+            max_volume = volume_ml
+            output_volume = volume_ml
+        else:
+            output_volume = max_volume
+            d_volume = 0
+
+        # trend of volume change
+        dd_volume = d_volume - d_volume_old
+        d_volume_old = d_volume
+
+    # print to console
+    print(int(output_volume))
+    print(int(dd_volume))
+
+
--- read_arduino_threading.py
+++ read_arduino_threading.py
@@ -1,28 +1,33 @@
 import serial
 import threading
+import time
 
 running = threading.Event()
 running.set()
+
 
 def thread_read(ser, callback=None):
     buf = b''
     while running.is_set():
         buf = read_data(ser, buf, callback)
 
+
 def msg_parsed(msg_parts):
     # Do something with the parsed data
     print(msg_parsed)
 
+
 ser = serial.Serial("COM3", 9600)
 th = threading.Thread(target=thread_read, args=(ser, msg_parsed))
 th.start()
+
 
 def read_data(ser, buf=b'', callback=None):
     if callback is None:
         callback = print
 
     # Read enough data for a message
-    buf += ser.read(ser.inwaiting()) # If you are using threading +10 or something so the thread has to wait for more data, this makes the thread sleep and allows the main thread to run.
+    buf += ser.read(ser.inwaiting())  # If you are using threading +10 or something so the thread has to wait for more data, this makes the thread sleep and allows the main thread to run.
     while b"[" not in buf or b"]" not in buf:
         buf += ser.read(ser.inwaiting())
 
@@ -39,11 +44,12 @@ def read_data(ser, buf=b'', callback=None):
         if msg_parts[-1] == b"1234": # There are many different ways to make a good checksum
             callback(msg_parts[:-1])
 
-   return buf
+    #  return buf
+
 
 # Do other stuff while the thread is running in the background
 start = time.clock()
-duration = 5 # Run for 5 seconds
+duration = 5  # Run for 5 seconds
 while running.is_set():
     time.sleep(1) # Do other processing instead of sleep
     if time.clock() - start > duration:
